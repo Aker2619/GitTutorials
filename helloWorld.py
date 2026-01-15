695cb71a514644f0cf834131e1bdcfdbe913e655
Alonza <ibegiegungun@mac.mynetworksettings.com>
2026-01-15 10:21:20 -0500

    Hopefully fixed a bug on only the first name speaking, while the other names do not say anything

--- helloWorld.py
+++ helloWorld.py
@@ -3,10 +3,10 @@ import pyttsx3
 name = "Alonza"
 names = ["Alonza", "John", "Micheal", "Kelly", "Marlon"]
 
-engine = pyttsx3.init()
-
 for name in names:
+    engine = pyttsx3.init()
     greeting = f"Hello, {name}!"
     print(greeting)
     engine.say(greeting)
     engine.runAndWait()
+    engine.stop()
